b7b0043ae90abd89a2f735e09779bc7fa2040adf
Ethan Lajeunesse <ethan@lajfam.com>
2023-11-04 16:51:07 -0400

    Checkpoint

--- api/routes/game.js
+++ api/routes/game.js
@@ -4,13 +4,9 @@ const solanaWeb3 = require('@solana/web3.js');
 const { PublicKey, sign } = require('@solana/web3.js');
 const { RewardService } = require('../services/rewardService');
 
-router.post('/submit', function (req, res) {
-	try {
-		const { signature, address, message } = req.body;
-		RewardService.sendTokens(address, 1);
-	} catch (error) {
-		console.log(error)
-	}
+router.post('/submit', async function (req, res) {
+	const { signature, address, message } = req.body;
+	await RewardService.sendTokens(address, 1);
 
 	return res.send('Token awarded!');
 
--- api/services/rewardService.js
+++ api/services/rewardService.js
@@ -1,35 +1,73 @@
-const { Transaction, SystemProgram, PublicKey, LAMPORTS_PER_SOL, VersionedTransaction } = require('@solana/web3.js');
-const { Connection } = require('@solana/web3.js');
-const { Keypair } = require('@solana/web3.js');
+const { Transaction, PublicKey, LAMPORTS_PER_SOL, VersionedTransaction } = require('@solana/web3.js');
+const { Connection, Keypair } = require('@solana/web3.js');
+const tokenMintAddress = '7p2rajmPxWW98yZpvfGaEXZ16pwVczD6kKtCoPtejp9d';
+const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } = require('@solana/spl-token');
+const Token = require('@solana/spl-token');
 
 const connection = new Connection('https://api.devnet.solana.com');
 
 class RewardService {
-	static async sendTokens(recipientAddress, solAmount) {
+	static async getOrCreateAssociatedTokenAccount(ownerAddress) {
+		const associatedAddress = await Token.getAssociatedTokenAddress(
+			ASSOCIATED_TOKEN_PROGRAM_ID,
+			TOKEN_PROGRAM_ID,
+			new PublicKey(tokenMintAddress),
+			new PublicKey(ownerAddress)
+		);
+
+		const associatedAccountInfo = await connection.getAccountInfo(associatedAddress);
+		if (!associatedAccountInfo) {
+			const transaction = new Transaction().add(
+				Token.createAssociatedTokenAccountInstruction(
+					ASSOCIATED_TOKEN_PROGRAM_ID,
+					TOKEN_PROGRAM_ID,
+					new PublicKey(tokenMintAddress),
+					associatedAddress,
+					new PublicKey(ownerAddress),
+					new PublicKey(ownerAddress)
+				)
+			);
+
+			return {
+				transaction,
+				associatedAddress
+			};
+		} else {
+			return {
+				associatedAddress
+			};
+		}
+	}
+
+	static async sendTokens(recipientAddress, amount) {
 		const senderPrivateKey = new Uint8Array(process.env.SOL_PRIVATE_KEY.split(','));
 		const senderKeypair = Keypair.fromSecretKey(senderPrivateKey);
 
-		const transaction = new Transaction().add(
-			SystemProgram.transfer({
-				fromPubkey: senderKeypair.publicKey,
-				toPubkey: new PublicKey(recipientAddress),
-				lamports: this.solToLamports(solAmount),
-			})
+		const { transaction: associatedTx, associatedAddress } = await this.getOrCreateAssociatedTokenAccount(recipientAddress, tokenMintAddress);
+
+		const transferTx = Token.createTransferInstruction(
+			senderKeypair.publicKey,
+			new PublicKey(associatedAddress),
+			new PublicKey(recipientAddress),
+			amount,
+			[senderKeypair]
 		);
 
-		const latestBlockhashInfo = await connection.getLatestBlockhash();
-		transaction.recentBlockhash = latestBlockhashInfo.blockhash;
+		const combinedTransaction = new Transaction().add(associatedTx).add(transferTx);
 
-		transaction.sign(senderKeypair);
+		const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
 
-		const serializedTransaction = transaction.serialize();
+		combinedTransaction.recentBlockhash = blockhash;
+		combinedTransaction.sign(senderKeypair);
+
+		const serializedTransaction = combinedTransaction.serialize();
 		const versionedTransaction = VersionedTransaction.deserialize(serializedTransaction);
 
 		const txid = await connection.sendTransaction(versionedTransaction);
 
 		await connection.confirmTransaction({
-			blockhash: latestBlockhashInfo.blockhash,
-			lastValidBlockHeight: latestBlockhashInfo.lastValidBlockHeight,
+			blockhash,
+			lastValidBlockHeight,
 			signature: txid,
 		});
 
