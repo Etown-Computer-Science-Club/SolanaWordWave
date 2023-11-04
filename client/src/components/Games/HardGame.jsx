1b58abbe4b0d451f1d87884302d130412c1718a6
klinefelters <stevenjk03@gmail.com>
2023-11-04 16:57:07 -0400

    Hardgame working

--- client/src/components/Games/HardGame.jsx
+++ client/src/components/Games/HardGame.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Box, Text, Input, Button } from '@chakra-ui/react';
+import { Box, Text, Input, Button, Flex, Heading, Spacer } from '@chakra-ui/react';
 
-const HardGame = ({ onDefinitionSubmit }) => {
-  const [definition, setDefinition] = React.useState('');
+const HardGame = () => {
+  const [answer, setAnswer] = React.useState('');
+  const wordOfTheDay = "Velocity"
+  const partOfSpeach = "Noun"
+  const definition = "Velocity is a measure of an object's speed in a specific direction. It is a vector quantity that describes the rate of change of an object's position with respect to time and includes both the magnitude and direction of motion. Velocity is commonly expressed in units like meters per second (m/s) in the International System of Units (SI) and is essential in the field of physics to describe the motion of objects."
+  function onDefinitionSubmit () {
+    const data = {
+      word: wordOfTheDay,
+      difficulty: "hard",
+      answer: answer
+    }
+    console.log(`word: ${data.word} | diff: ${data.difficulty} | ans: ${data.answer} | `)
+  }
 
   return (
     <Box p={5} shadow="md" borderWidth="1px">
-      <Text fontSize="xl">Hard Mode: Use the Word</Text>
+      <Flex>
+        <Spacer />
+        <Heading fontSize="2xl">Hard Mode</Heading>
+        <Spacer />
+      </Flex>
+      <Text  fontSize="lg" py="2"></Text>
+      <Text fontSize="lg" fontWeight="bold" py="2">Word of the Day: {wordOfTheDay}</Text>
+      <Text fontSize="md" py="2">{partOfSpeach}: {definition}</Text>
       <Text mb={4}>Use the word correctly in a sentence.</Text>
       <Input 
         placeholder="Type your definition here..."
-        value={definition}
-        onChange={(e) => setDefinition(e.target.value)}
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
       />
-      <Button mt={4} colorScheme="teal" onClick={() => onDefinitionSubmit(definition)}>
+      <Button mt={4} colorScheme="teal" onClick={() => onDefinitionSubmit(answer)}>
         Submit
       </Button>
     </Box>
