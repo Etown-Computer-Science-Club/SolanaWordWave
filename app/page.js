e7a8f6e2078ccb7f731c9ae122654aab454df740
Ethan Lajeunesse <ethan@lajfam.com>
2023-11-04 06:05:21 -0400

    checkpoint

---
+++ app/page.js
@@ -0,0 +1,30 @@
+'use client';
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
+
+export const Home = (props) => {
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallet = new PhantomWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[wallet]}>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <p>Put the rest of your app here</p>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default Home;
