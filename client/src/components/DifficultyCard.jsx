687364f12d99ecea53d442dd82946e298410cd98
klinefelters <stevenjk03@gmail.com>
2023-11-05 00:56:19 -0400

    added svgs

--- client/src/components/DifficultyCard.jsx
+++ client/src/components/DifficultyCard.jsx
@@ -4,6 +4,9 @@ import {
   Text,
   Button,
   Heading,
+  Image,
+  Spacer,
+  Flex
 } from '@chakra-ui/react';
 
 const DifficultyCard = ({ title, description, tokens, handleGameSelect }) => {
@@ -21,6 +24,13 @@ const DifficultyCard = ({ title, description, tokens, handleGameSelect }) => {
 			<Text fontSize="2xl" fontWeight="bold" mb={4}>
 				Win {tokens} tokens
 			</Text>
+			<Flex my="8">
+				<Spacer/>
+				<Flex bg="#c8c8c8" borderRadius="2xl" w="25vh" h="25vh" justifyContent="center">
+					<Image h="25vh" py="4" src={`/${title}.svg`} fill="blue"/>
+				</Flex>
+				<Spacer/>
+			</Flex >
 			<Button colorScheme="purple" onClick={handleGameSelect}>Choose {title}</Button>
 		</Box>
 	);
--- client/src/components/Games/WordDescription.jsx
+++ client/src/components/Games/WordDescription.jsx
@@ -8,9 +8,9 @@ export default function WordDescription({ name, wordOfTheDay, partOfSpeech, defi
         <Heading fontSize="2xl">{name}</Heading>
         <Spacer />
       </Flex>
-      <Text fontSize="lg" py="2"></Text>
-      <Text fontSize="lg" fontWeight="bold" py="2">Word of the Day: {wordOfTheDay}</Text>
-      <Text fontSize="md" py="2">{partOfSpeech}: {definition}</Text>
+      <Text fontSize="2xl" py="2"></Text>
+      <Text fontSize="2xl" fontWeight="bold" py="2">Word of the Day: {wordOfTheDay}</Text>
+      <Text fontSize="xl" py="2">{partOfSpeech}: {definition}</Text>
     </Box>
   );
 }
